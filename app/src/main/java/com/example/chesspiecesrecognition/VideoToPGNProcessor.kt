496e3bb88642d8a96a245d5fb8234a1b9f4f61d4
Aleksandr Kalinin <akalinin@sfedu.ru>
2025-06-27 10:03:33 +0300

    fixed correctFenSequence

--- app/src/main/java/com/example/chesspiecesrecognition/VideoToPGNProcessor.kt
+++ app/src/main/java/com/example/chesspiecesrecognition/VideoToPGNProcessor.kt
@@ -10,6 +10,7 @@ import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.github.bhlangonijr.chesslib.Board
+import com.github.bhlangonijr.chesslib.Piece
 import com.github.bhlangonijr.chesslib.Square
 import com.github.bhlangonijr.chesslib.move.Move
 import org.tensorflow.lite.Interpreter
@@ -22,32 +23,13 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class VideoToPGNProcessor(private val context: Context, private val tfLiteInterpreter: Interpreter) {
-
-    companion object {
-        private val CHESS_POSITIONS = mapOf(
-            0 to "A8", 1 to "B8", 2 to "C8", 3 to "D8", 4 to "E8", 5 to "F8", 6 to "G8", 7 to "H8",
-            8 to "A7", 9 to "B7", 10 to "C7", 11 to "D7", 12 to "E7", 13 to "F7", 14 to "G7", 15 to "H7",
-            16 to "A6", 17 to "B6", 18 to "C6", 19 to "D6", 20 to "E6", 21 to "F6", 22 to "G6", 23 to "H6",
-            24 to "A5", 25 to "B5", 26 to "C5", 27 to "D5", 28 to "E5", 29 to "F5", 30 to "G5", 31 to "H5",
-            32 to "A4", 33 to "B4", 34 to "C4", 35 to "D4", 36 to "E4", 37 to "F4", 38 to "G4", 39 to "H4",
-            40 to "A3", 41 to "B3", 42 to "C3", 43 to "D3", 44 to "E3", 45 to "F3", 46 to "G3", 47 to "H3",
-            48 to "A2", 49 to "B2", 50 to "C2", 51 to "D2", 52 to "E2", 53 to "F2", 54 to "G2", 55 to "H2",
-            56 to "A1", 57 to "B1", 58 to "C1", 59 to "D1", 60 to "E1", 61 to "F1", 62 to "G1", 63 to "H1"
-        )
-
-        private val LABEL_MAP = mapOf(
-            0 to "bishop:Black", 1 to "king:Black", 2 to "knight:Black", 3 to "pawn:Black", 4 to "queen:Black", 5 to "rook:Black",
-            6 to "bishop:White", 7 to "king:White", 8 to "knight:White", 9 to "pawn:White", 10 to "queen:White", 11 to "rook:White",
-            12 to "empty:White"
-        )
-    }
-
     fun processVideoToPGN(videoUri: Uri, callback: (String) -> Unit) {
         try {
             val framesDir = extractFramesFromVideo(videoUri)
             val fens = recognizeFensFromFrames(framesDir)
-           // Log.d("FEN_LIST", "FENs: ${fens.joinToString("\n")}")
+            // Log.d("FEN_LIST", "FENs: ${fens.joinToString("\n")}")
             val correctedFens = correctFenSequence(fens)
+            //Log.d("COR_FEN_LIST", "FENs: ${correctedFens.joinToString("\n")}")
             val pgn = generatePGN(correctedFens)
 
             callback(pgn)
@@ -247,15 +229,33 @@ class VideoToPGNProcessor(private val context: Context, private val tfLiteInterp
 
 
     private fun correctFenSequence(fens: List<String>): List<String> {
+
         if (fens.isEmpty()) return emptyList()
 
         val board = Board()
         board.loadFromFen(fens[0])
-        val corrected = mutableListOf(board.fen)
+        val corrected = mutableListOf(normalizeFen(board.fen))
+        val recognitionErrors = mutableSetOf<Triple<Square, Piece?, Piece?>>()
 
         for (i in 1 until fens.size) {
             val tempBoard = Board()
             tempBoard.loadFromFen(fens[i])
+
+            // Находим изменения
+            val changes = mutableListOf<Triple<Square, Piece?, Piece?>>()
+            for (square in Square.entries) {
+                val prevPiece = board.getPiece(square)
+                val currPiece = tempBoard.getPiece(square)
+                if (prevPiece != currPiece) {
+                    if (prevPiece.toString() != currPiece.toString().lowercase()) {
+                        if (!recognitionErrors.contains(Triple(square, prevPiece, currPiece)))
+                            changes.add(Triple(square, prevPiece, currPiece))
+                    }
+                }
+            }
+
+            if (changes.count() == 0) continue
+
             var bestMove: Move? = null
             var bestDiffCount = Int.MAX_VALUE
 
@@ -264,26 +264,40 @@ class VideoToPGNProcessor(private val context: Context, private val tfLiteInterp
                 testBoard.loadFromFen(board.fen)
                 testBoard.doMove(move)
 
-                val diffCount = Square.values().count { square ->
-                    testBoard.getPiece(square) != tempBoard.getPiece(square)
+                var diffCount = 0
+                for (square in Square.entries) {
+                    val targetPiece = tempBoard.getPiece(square)
+                    val testPiece = testBoard.getPiece(square)
+                    if (targetPiece != testPiece) {
+                        diffCount++
+                    }
                 }
 
-                if (diffCount < bestDiffCount) {
+                if (diffCount <= 2) {
                     bestDiffCount = diffCount
                     bestMove = move
                     if (diffCount == 0) break
                 }
             }
-
             if (bestMove != null && bestDiffCount <= 2) {
                 board.doMove(bestMove)
-                corrected.add(board.fen)
-            } else {
-                corrected.add(board.fen)
+                corrected.add(normalizeFen(board.fen))
+            }
+            else {
+                for (j in 0 until changes.size) {
+                    recognitionErrors.add(changes[j])
+                }
+                corrected.add(normalizeFen(board.fen))
             }
         }
 
         return corrected.distinct()
+    }
+
+    private fun normalizeFen(fen: String): String {
+        // Удаляем информацию о количестве ходов и полуходов, оставляя только позицию и очередь хода
+        val parts = fen.split(" ")
+        return "${parts[0]} ${parts[1]} - - 0 1"
     }
 
     private fun generatePGN(fens: List<String>): String {
